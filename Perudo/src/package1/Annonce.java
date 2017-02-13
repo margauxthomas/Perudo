d7f2ba642ddc99fe15bb4700d40b7230a449abd3
jonat <jonat@172.17.58.232>
2017-02-13 19:22:45 +0100

    OSEF DU COMMIT V2 A UN MOMENT CA VA MARCHER JCROIS

--- Perudo/src/package1/Annonce.java
+++ Perudo/src/package1/Annonce.java
@@ -4,10 +4,35 @@ import java.rmi.RemoteException;
 import java.util.*;
 import java.io.Serializable;
 
-/*
- *
- * @author jonat
- */
+public class Annonce implements Serializable{    
+
+    private int nbDé;
+    private int valDé;
+    
+    //Constructeur
+    public Annonce (Integer nbDé, Integer valDé) {
+        this.nbDé= nbDé;
+        this.valDé = valDé;
+    }  
+    public int getDé() {
+        return nbDé;
+    }    
+    
+    public int getAnnValDé() {
+        return valDé;
+    }    
+    
+    public void setAnnonce(Integer nbDé, Integer valDé) {
+        this.nbDé = nbDé;
+        this.valDé = valDé;
+    }
+     
+    private static Vector classes = new Vector();
+    public static Annonce getInstanceA(Integer nbdé, Integer valDé){
+=======
+     
+    private static Vector classes = new Vector();
+    public static Annonce getInstanceA(Integer nbdé, Integer valDé){
 public class Annonce implements Serializable{
     
       private int nbDé;
@@ -43,27 +68,6 @@ public class Annonce implements Serializable{
      
     private static Vector classes = new Vector();
     public static Annonce getInstanceA(Integer nbdé, Integer valDé){
->>>>>>> origin/master
-            
-<<<<<<< HEAD
-		Annonce tmp = new Annonce(nbdé, valDé);
-		if (classes.contains(tmp)) {
-			Enumeration enume = classes.elements();
-			while (enume.hasMoreElements()) {
-				Annonce element = (Annonce) enume.nextElement();
-				if (element.equals(tmp)) {
-					return element;
-				}
-			}
-		}
-		else {
-			classes.add(tmp);
-			return tmp;
-		}
-                    return null;
-	}*/
-}
-
         Annonce tmp = new Annonce(nbdé, valDé);
         if (classes.contains(tmp)) {
             Enumeration enume = classes.elements();
@@ -82,4 +86,3 @@ public class Annonce implements Serializable{
     }
 
 }
-
